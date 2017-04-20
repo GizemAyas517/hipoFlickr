7c8fd941c99c829543c90ec381954de0683c9f1d
Gizem Ayas <gayas14@ku.edu.tr>
2017-04-20 23:37:43 +0300

    Sample image display using URLs

---
+++ app/src/main/java/com/example/gizem/flikr/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.gizem.flikr;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] images = {
+            "https://www.looktheapp.com/api/pictures/pic-1.jpg",
+            "https://www.looktheapp.com/api/pictures/pic-2.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-3.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-3.jpg",
+            "https://www.looktheapp.com/api/pictures/pic-4.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-5.jpg",
+            "https://www.looktheapp.com/api/pictures/pic-6.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-7.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-8.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-9.jpeg",
+            "https://www.looktheapp.com/api/pictures/pic-10.jpg",
+            "https://www.looktheapp.com/api/pictures/pic-11.jpeg"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TableLayout layout= (TableLayout) findViewById(R.id.myLayout);
+        TextView tv= new TextView(this);
+        tv.setText("abv");
+        layout.addView(tv);
+        for (String image : images) {
+            ImageView im = new ImageView(this);
+            Picasso.with(this).load(image).into(im);
+            layout.addView(im);
+            Log.i("IMAGE", image);
+        }
+
+    }
+
+
+}
